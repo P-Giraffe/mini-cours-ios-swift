c4cef7707d4ddc2d20711fea23ba1e19558f2728
Maxime Britto <maxbritto@users.noreply.github.com>
2017-06-28 14:28:43 +0200

    Hello World : bouton action
    -Connexion du bouton "Répéter" avec le code en swift via une Action IB

--- hello_world/hello_world/ViewController.swift
+++ hello_world/hello_world/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func repeatButtonTouched() {
+        print("Bouton répéter touché par l'utilisateur")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
