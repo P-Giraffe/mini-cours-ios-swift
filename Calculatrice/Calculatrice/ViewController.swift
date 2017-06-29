be545b459c171da3ab27dc854e11dcbad878af48
Maxime Britto <maxbritto@users.noreply.github.com>
2017-06-29 14:07:47 +0200

    Calculatrice : projet et IHM
    Creation du projet initial
    Creation de l'IHM minimaliste

---
+++ Calculatrice/Calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by Maxime Britto on 29/06/2017.
+//  Copyright © 2017 mbritto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
